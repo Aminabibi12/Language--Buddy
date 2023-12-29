d70bc257391336281086417580c51393f5f14a30
occ-113 <occ-113@occ-113s-MacBook-Pro.local>
2023-12-29 19:51:50 +0100

    new commit

--- frontend-part/src/config.js
+++ frontend-part/src/config.js
@@ -14,7 +14,7 @@
 
 function config() {
     return {
-        baseUrl: "https://language-buddy-frontend.vercel.app/",
+        baseUrl: "https://localhost:4000",
 
         headers: {
             'Content-Type': 'application/json'
