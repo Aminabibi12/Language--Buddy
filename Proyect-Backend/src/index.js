738b887005bd90dbaaccc112dd0f989cdeb55c9b
occ-113 <occ-113@occ-113s-MacBook-Pro.local>
2024-01-31 18:46:46 +0100

    fourth commit

--- Proyect-Backend/src/index.js
+++ Proyect-Backend/src/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 // Middlewares
 app.use(cors({
-  origin: 'language-buddy-alpha.vercel.app',
+  origin: 'http://localhost:3000/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
