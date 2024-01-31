757f300f08218878fa1146f79d37c6bb27ce885a
occ-113 <occ-113@occ-113s-MacBook-Pro.local>
2024-01-31 18:44:50 +0100

    fourth commit

--- frontend-part/src/config.js
+++ frontend-part/src/config.js
@@ -1,26 +1,26 @@
-// function config() {
-//     if (process.env.NODE_ENV == "production") {
-//         return {
-//             baseUrl: "https://language-buddy.herokuapp.com"
-//         }
-//     } else {
-//         return {
-//             baseUrl: "http://localhost:4000"
-//         }
-//     }
-// }
-
-// export default config()
-
 function config() {
-    return {
-        baseUrl: "https://build-nine-peach.vercel.app",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        credentials: { include: true }
-
-    };
+    if (process.env.NODE_ENV == "production") {
+        return {
+            baseUrl: "https://build-nine-peach.vercel.app"
+        }
+    } else {
+        return {
+            baseUrl: "http://localhost:4000"
+        }
+    }
 }
 
-export default config();
+export default config()
+
+// function config() {
+//     return {
+//         baseUrl: "https://build-nine-peach.vercel.app",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         credentials: { include: true }
+
+//     };
+// }
+
+// export default config();
